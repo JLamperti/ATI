80cab46a15a3e6016abea1851ff4de2221022d7c
Pascal Stieglitz <pascal.stieglitz@gmail.com>
2017-12-03 14:59:15 +0100

    vorschlag fuer routing

--- ati-website/app.js
+++ ati-website/app.js
@@ -5,8 +5,11 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+//import the routers
 var index = require('./routes/index');
-var users = require('./routes/users');
+var users = require('./routes/profile');
+var form = require('./routes/form');
+var results = require('./routes/results');
 
 var app = express();
 
@@ -22,8 +25,11 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+//set up routing
 app.use('/', index);
-app.use('/users', users);
+app.use('/profile', profile);
+app.use('/results', results);
+app.use('/form', form);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
