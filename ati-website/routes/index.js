19908f0f17820889f136665530f21eceb095ef69
Jonas Lamperti <jonas.lamperti@web.de>
2017-12-05 19:31:59 +0100

    added text to index + option to download scale

--- ati-website/routes/index.js
+++ ati-website/routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
+/*
+router.get('/dl', function(req, res){
+  res.send(./public/files/skala.pdf);
+});*/
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'ATI' });
 });
 
 module.exports = router;
