ea9007ec5e21c8e4f320b2e0866e1598292f8926
Pascal Stieglitz <pascal.stieglitz@gmail.com>
2017-12-10 17:49:06 +0100

    fixed dl

--- ati-website/routes/index.js
+++ ati-website/routes/index.js
@@ -1,10 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
-/*
-router.get('/dl', function(req, res){
-  res.send(./public/files/skala.pdf);
-});*/
+
+router.use('/dl', function(req, res){
+  res.download('public/files/skala.pdf');
+});
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'ATI' });
