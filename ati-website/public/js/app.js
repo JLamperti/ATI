44c9b94ab6f10095e76eda6baf3bd08f880fbd49
Jeffrey <jeffrey.pluntke@student.uni-luebeck.de>
2018-01-13 17:34:19 +0100

    created clientside cookie

---
+++ ati-website/public/js/app.js
@@ -0,0 +1,22 @@
+//create a cookie
+console.log(document.cookie);
+function setLang() {
+  
+    //get the language of the Browser
+    let userLang =
+      navigator.language || navigator.userLanguage.toString();
+    let val = userLang == "en-US" ? "en" : "de";
+    //create cookie
+    document.cookie = "locale=" + val + "";
+ 
+}
+//change the language and reload the site
+function changeLang() {
+  if (document.cookie.toString() == "locale=en") {
+    document.cookie = "locale=de";
+  } else if (document.cookie.toString() == "locale=de") {
+    document.cookie = "locale=en";
+  }
+  console.log(document.cookie);
+  location.reload();
+}
