d3fa39edaa910353f60aeb203b56ad74b483374a
Jonas Lamperti <jonas.lamperti@web.de>
2018-01-22 15:34:47 +0100

    togglebutton, renamed profile to user

--- ati-website/public/js/user.js
+++ ati-website/public/js/user.js
@@ -1,6 +1,26 @@
-$(document).ready(function(){
-  $("#toggleDetails").click(function(){
-    console.log('pressed button');
+$(document).ready(function() {
+  $("#toggleDetails").click(function() {
+
+    if (document.getElementById('toggleDetails').innerHTML == '<p><span class="glyphicon glyphicon-menu-down"></span></p>') {
+      document.getElementById('toggleDetails').innerHTML = ('<p><span class="glyphicon glyphicon-menu-up"></span></p>');
+    } else {
+      document.getElementById('toggleDetails').innerHTML= ('<p><span class="glyphicon glyphicon-menu-down"></span></p>');
+    }
     $("#details").slideToggle();
+
+    /*$(this).html(function(v) {
+      return v === "<p><span class='glyphicon glyphicon-menu-down'></span></p>" ? "a" : "<p><span class='glyphicon glyphicon-menu-up'></span></p>"
+    });*/
   });
 });
+/*
+if ("glyphicon-menu-down".indexOf(document.getElementById('toggleDetails').innerHTML)) {
+  $('#toggleDetails').text("<p><span class='glyphicon glyphicon-menu-up'></span></p>");
+} else {
+  document.getElementById('toggleDetails').innerHTML = "<p><span class='glyphicon glyphicon-menu-down'></span></p>";
+}
+*/
+/*$("#toggleDetails").click(
+  $("#details").slideToggle();
+);
+*/
