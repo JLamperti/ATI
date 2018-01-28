30d779263fddd4675845dc854d09e651ff82e265
Pascal Stieglitz <pascal.stieglitz@gmail.com>
2018-01-28 14:18:20 +0100

    fixed minor problem

--- ati-website/custom_modules/sched.js
+++ ati-website/custom_modules/sched.js
@@ -13,37 +13,37 @@ var sched = schedule.scheduleJob('1 0 * * *', function() {
 	let d = new Date().getFullYear() + '-' + new Date().getMonth()+1 + '-' + new Date().getDate();
 	dba.performQuery('DELETE FROM link WHERE ExpirationDate <= \'' + d + '\';', function (err, result) {
 		if (err || result == undefined) {
-			return console.log('Err: Bad query. (app.js:sched)');
+			return console.log('Err: Bad query. (sched.js:1)');
 		}
 	});
 	dba.performQuery('DELETE FROM link WHERE UsesLeft=0;', function (err, result) {
 		if (err || result == undefined) {
-			return console.log('Err: Bad query. (app.js:sched)');
+			return console.log('Err: Bad query. (sched.js:2)');
 		}
 	});
 	dba.performQuery('UPDATE Survey SET surveyStatus = \'closed\' WHERE (SELECT COUNT(PID) >= MaxProbands FROM partOf WHERE SID = SurveyID);', function (err, result) {
 		if (err || result == undefined) {
-			return console.log('Err: Bad query. (app.js:sched)');
+			return console.log('Err: Bad query. (sched.js:3)');
 		}
 	});
 	dba.performQuery('UPDATE Survey SET surveyStatus = \'closed\' WHERE surveyEnd = \'' + d + '\';', function (err, result) {
 		if (err || result == undefined) {
-			return console.log('Err: Bad query. (app.js:sched)');
+			return console.log('Err: Bad query. (sched.js:4)');
 		}
 	});
 	dba.performQuery('UPDATE Survey SET surveyStatus = \'open\' WHERE surveyBegin = \'' + d + '\';', function (err, result) {
 		if (err || result == undefined) {
-			return console.log('Err: Bad query. (app.js:sched)');
+			return console.log('Err: Bad query. (sched.js:5)');
 		}
 	});
 	dba.performQuery('DELETE FROM bestaetigungslinks WHERE ExpirationDate <= \'' + d + '\';', function (err, result) {
 		if (err || result == undefined) {
-			return console.log('Err: Bad query. (app.js:sched)');
+			return console.log('Err: Bad query. (sched.js:6)');
 		}
 	});
 	dba.performQuery('DELETE FROM pwlinks WHERE ExpirtionDate <= \'' + d + '\';', function (err, result) {
 		if (err || result == undefined) {
-			return console.log('Err: Bad query. (app.js:sched)');
+			return console.log('Err: Bad query. (sched.js:7)');
 		}
 	});
 });
