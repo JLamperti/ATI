6d3d0952ffd0493b8fbac2e8a9e8c4588c051acb
Jonas Lamperti <jonas.lamperti@student.uni-luebeck.de>
2018-03-13 16:35:28 +0100

    secured surveyDetails against unauthed access

--- ati-website/routes/user.js
+++ ati-website/routes/user.js
@@ -32,16 +32,46 @@ router.get('/newSurvey', function(req, res, next) {
 
 
 router.get('/surveyDetails/:id([0-9]{1,20})', function(req, res, next) {
-  if (req.session.user && req.cookies.user_sid) {
-	  let id = req.params.id;
-    res.render('surveyDetails', {
-      title: 'ATI',
-      surveyId: id
-    });
-  } else {
-    res.render('needToLogin');
-    // res.redirect('login');
-  }
+  dba.performQuery('SELECT IF (' + req.query.SID + ' in (SELECT surveyID FROM survey), true, false) AS b;', function(err, result) {
+    if (err) {
+      res.status(404).send('Survey not found.');
+      return console.log('Survey not found');
+    }
+    if (JSON.parse(JSON.stringify(result))[0].b == 1) {
+      dba.performQuery('SELECT IF(UID=' + req.session.user + ', true, false) AS b FROM Survey WHERE surveyID=' + req.query.SID + ';', function(err, result) {
+        if (err || result == undefined) {
+          res.status(500).send('Something went wrong');
+          return console.log('user.js:get surveyDetails ' + err.toString());
+        }
+        let string = JSON.stringify(result);
+        let json = JSON.parse(string);
+        if (json[0].b) {
+          res.render('surveyDetails', {
+            title: 'ATI',
+            surveyId: id
+          });
+        } else {
+          res.status(403).send('Not permitted for you.');
+        }
+      });
+    } else {
+      res.status(404).send('Survey not found');
+    }
+  });
+} else {
+  res.render('needToLogin');
+}
+});
+if (req.session.user && req.cookies.user_sid) {
+  let id = req.params.id;
+  res.render('surveyDetails', {
+    title: 'ATI',
+    surveyId: id
+  });
+} else {
+  res.render('needToLogin');
+  // res.redirect('login');
+}
 });
 
 /* GET profile page. */
