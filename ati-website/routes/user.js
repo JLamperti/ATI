98117dac803e3dead579dac560a070feac022d4a
Pascal Stieglitz <pascal.stieglitz@gmail.com>
2018-03-17 12:52:11 +0100

    blocked surveydetails for wrong user

--- ati-website/routes/user.js
+++ ati-website/routes/user.js
@@ -33,11 +33,25 @@ router.get('/newSurvey', function(req, res, next) {
 
 router.get('/surveyDetails/:id([0-9]{1,20})', function(req, res, next) {
   if (req.session.user && req.cookies.user_sid) {
-	  let id = req.params.id;
-    res.render('surveyDetails', {
-      title: 'ATI',
-      surveyId: id
-    });
+		let id = req.params.id;
+		dba.performQuery('SELECT IF(UID=' + req.session.user + ', true, false) AS b FROM Survey WHERE surveyID=' + id + ';', function(err, result) {
+			if (err || result == undefined) {
+				res.status(500);
+				res.render('error', { message: 'Something went wrong.' , error: new Error(500)});
+				return console.log('user.js:get surveyDetails ' + err.toString());
+			}
+			let string = JSON.stringify(result);
+			let json =  JSON.parse(string);
+			if (json[0].b) {
+				res.render('surveyDetails', {
+				title: 'ATI',
+				surveyId: id
+				});
+			} else {
+				res.status(403);
+				res.render('error', { message: 'You are not permitted to access this survey.' , error: new Error(403)  });
+			}
+	  });
   } else {
     res.render('needToLogin');
     // res.redirect('login');
