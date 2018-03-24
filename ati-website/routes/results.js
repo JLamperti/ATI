bf5dfe65760278c08bfdb8b9c4f90fa83e0a7a16
Pascal Stieglitz <pascal.stieglitz@gmail.com>
2018-03-24 10:35:27 +0100

    removed /result from the website

--- ati-website/routes/results.js
+++ ati-website/routes/results.js
@@ -2,8 +2,9 @@ var express = require('express');
 var router = express.Router();
 
 /* GET results listing. */
-router.get('/', function(req, res, next) {
+//this route is (at least currently not needed. TRhis entire file therefor has no use.
+/*router.get('/', function(req, res, next) {
   res.render('result', { title: 'ATI', atiScore: 5 });
-});
+});*/
 
 module.exports = router;
